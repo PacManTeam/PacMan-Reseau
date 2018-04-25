1443484b75aed38902ac1a820f1672bfc0760ef0
GermainJonathan <jgermain@gismartware.com>
2018-04-25 10:51:31 +0200

    :boom: Score - Création du score du joueur

--- Assets/Scripts/pacdot.cs
+++ Assets/Scripts/pacdot.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pacdot : MonoBehaviour {
+    public Text scoreUI;
 
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.name == "pacman")
+        {
+            co.GetComponent<scoreJoueur>().score++;
             Destroy(gameObject);
+            scoreUI.text = "Score : " + co.GetComponent<scoreJoueur>().score.ToString();
+        }
+            
         Debug.Log(co);
     }
 }
