3abb8de67fe506e6abc6ab5723c093cd3e959994
GermainJonathan <jgermain@gismartware.com>
2018-05-23 21:04:46 +0200

    :hankey: Test de scipt reseau

---
+++ Assets/Scripts/multiMode/networkingStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class networkingStart : NetworkManager
+{
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Debug.Log(conn);
+        Debug.Log(playerControllerId);
+        base.OnServerAddPlayer(conn, playerControllerId);
+    }
+}
--- Assets/Scripts/multiMode/pacmanMoveMulti.cs
+++ Assets/Scripts/multiMode/pacmanMoveMulti.cs
@@ -18,67 +18,69 @@ public class pacmanMoveMulti : NetworkBehaviour {
     // Use this for initialization
     public void Start()
     {
-        dest = transform.position;
-        GetComponent<pacmanPlayer>().spawnPosition = dest;
-        currentMove.Add("top", false);
-        currentMove.Add("bottom", false);
-        currentMove.Add("right", false);
-        currentMove.Add("left", false);
+        this.dest = transform.position;
+        this.currentMove.Add("top", false);
+        this.currentMove.Add("bottom", false);
+        this.currentMove.Add("right", false);
+        this.currentMove.Add("left", false);
     }
 
     // Update is called once per frame
-    public void FixedUpdate()
+    public void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if(!isLocalPlayer)
         {
-            SceneManager.UnloadSceneAsync("soloMode");
-            SceneManager.LoadScene("mainMenu");
-        }
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.UnloadSceneAsync("multiMode");
+                SceneManager.LoadScene("mainMenu");
+            }
 
-        // Move closer to Destination
-        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
+            // Move closer to Destination
+            Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
 
-        // Check for Input if not moving
-        if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-        {
-            resetInput();
-            currentMove["top"] = true;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-        {
-            resetInput();
-            currentMove["right"] = true;
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-        {
-            resetInput();
-            currentMove["bottom"] = true;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-        {
-            resetInput();
-            currentMove["left"] = true;
-        }
+            // Check for Input if not moving
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+            {
+                resetInput();
+                currentMove["top"] = true;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+            {
+                resetInput();
+                currentMove["right"] = true;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+            {
+                resetInput();
+                currentMove["bottom"] = true;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+            {
+                resetInput();
+                currentMove["left"] = true;
+            }
 
-        if ((Vector2)transform.position == dest)
-        {
-            if (nextDist != Vector2.zero)
-                dest = (valid(nextDist)) ? (Vector2)transform.position + nextDist : dest;
-            if (currentMove["top"])
-                dest = (valid(Vector2.up)) ? (Vector2)transform.position + Vector2.up : dest;
-            if (currentMove["right"])
-                dest = (valid(Vector2.right)) ? (Vector2)transform.position + Vector2.right : dest;
-            if (currentMove["bottom"])
-                dest = (valid(-Vector2.up)) ? (Vector2)transform.position - Vector2.up : dest;
-            if (currentMove["left"])
-                dest = (valid(-Vector2.right)) ? (Vector2)transform.position - Vector2.right : dest;
-        }
+            if ((Vector2)transform.position == dest)
+            {
+                if (nextDist != Vector2.zero)
+                    dest = (valid(nextDist)) ? (Vector2)transform.position + nextDist : dest;
+                if (currentMove["top"])
+                    dest = (valid(Vector2.up)) ? (Vector2)transform.position + Vector2.up : dest;
+                if (currentMove["right"])
+                    dest = (valid(Vector2.right)) ? (Vector2)transform.position + Vector2.right : dest;
+                if (currentMove["bottom"])
+                    dest = (valid(-Vector2.up)) ? (Vector2)transform.position - Vector2.up : dest;
+                if (currentMove["left"])
+                    dest = (valid(-Vector2.right)) ? (Vector2)transform.position - Vector2.right : dest;
+            }
 
-        // Animation Parameters
-        Vector2 dir = dest - (Vector2)transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
+            // Animation Parameters
+            Vector2 dir = dest - (Vector2)transform.position;
+            GetComponent<Animator>().SetFloat("DirX", dir.x);
+            GetComponent<Animator>().SetFloat("DirY", dir.y);
+        }
     }
 
     public bool valid(Vector2 dir)
