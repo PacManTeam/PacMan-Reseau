bad903c83e7eef6e21029feda70cc20167f03656
GermainJonathan <jgermain@gismartware.com>
2018-05-24 07:50:03 +0200

    :construction: Auth - Page de connexion

---
+++ Assets/Scripts/menu/authentification.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class authentification : MonoBehaviour {
+
+    public void editFinish()
+    {
+        Debug.Log("Test");
+    }
+
+    public void tryToConnect()
+    {
+        Debug.Log("Connection");
+        SceneManager.LoadScene("mainMenu");
+    }
+}
