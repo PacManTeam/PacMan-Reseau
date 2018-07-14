22feb10016583c5f5c77f093ef077627dd703db9
GermainJonathan <jgermain@gismartware.com>
2018-07-14 23:48:39 +0200

    :+1: Connection avec le serveur NodeJS

--- Assets/Scripts/menu/authentification.cs
+++ Assets/Scripts/menu/authentification.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class authentification : MonoBehaviour {
 
     public void editFinish()
     {
-        Debug.Log("Test");
+
     }
 
     public void tryToConnect()
     {
-        Debug.Log("Connection");
-        SceneManager.LoadScene("mainMenu");
+        Debug.Log("tryTo");
+        StartCoroutine(this.testAuth());
+    }
+
+    private IEnumerator testAuth()
+    {
+        Debug.Log("Authentification");
+        WWWForm form = new WWWForm();
+        form.AddField("login", "admin");
+        form.AddField("password", "admin");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:3000/authentifier", form))
+        {
+            Debug.Log(www);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                SceneManager.LoadScene("mainMenu");
+            }
+        }
     }
 }
